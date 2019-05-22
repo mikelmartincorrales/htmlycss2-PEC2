287c7cd3dc19660863d11664d104b7b3a84d4691
Mikel Martín <mikel.martin@edooca.com>
2019-05-22 13:08:20 +0200

    js libs en local

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -5,9 +5,9 @@
 
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
-import "./node_modules/jquery/dist/jquery.min.js";
-import "./node_modules/popper.js/dist/umd/popper.min.js";
-import "./node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "../../../node_modules/jquery/dist/jquery.min.js";
+import "../../../node_modules/popper.js/dist/umd/popper.min.js";
+import "../../../node_modules/bootstrap/dist/js/bootstrap.min.js";
 
 /**
  * Write any other JavaScript below
