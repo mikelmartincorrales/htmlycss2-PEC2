d0093cd037ab8eea01cd62242daa0c217b761b97
Mikel Martín <mmartincorr@uoc.edu>
2019-05-22 13:52:28 +0200

    formato

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,8 +3,6 @@
  * see commented examples below
  */
 
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
 import "../../../node_modules/jquery/dist/jquery.min.js";
 import "../../../node_modules/popper.js/dist/umd/popper.min.js";
 import "../../../node_modules/bootstrap/dist/js/bootstrap.min.js";
